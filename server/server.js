e8996e149c4e4b58f9de701bd350d3d8bfbe2c60
Antoine Arlaud <antoine@newyorkbeesanctuary.org>
2015-08-02 22:30:54 +0000

    First Commit of the broker file. Using vanilla Mosca broker (mosca.io) configuration with mongodb persistence setup in server/server.js

---
+++ server/server.js
@@ -0,0 +1,35 @@
+var mosca = require('mosca')
+
+var pubsubsettings = {
+  type: 'mongo',        
+  url: 'mongodb://localhost:27000/mqtt',
+  pubsubCollection: 'mqttCollection',
+  mongo: {}
+};
+
+var moscaSettings = {
+  port: 1883,
+  backend: pubsubsettings,
+  persistence: {
+    factory: mosca.persistence.Mongo,
+    url: 'mongodb://localhost:27000/mqtt'
+  }
+};
+
+var server = new mosca.Server(moscaSettings);
+server.on('ready', setup);
+
+server.on('clientConnected', function(client) {
+    console.log('client connected', client.id);     
+});
+
+// fired when a message is received
+server.on('published', function(packet, client) {
+  console.log('Published', packet.payload);
+  console.log('Published topic: ', packet.topic);
+});
+
+// fired when the mqtt server is ready
+function setup() {
+  console.log('Mosca server is up and running')
+}
